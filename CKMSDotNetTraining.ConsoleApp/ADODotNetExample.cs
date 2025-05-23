1d6417c1f205a1d6a04c2bdc4bba26651017e5a0
ChanKhaingMarShwe <chankhaingmarshwe@gmail.com>
2025-05-23 09:33:18 +0630

    ado.net Create

---
+++ CKMSDotNetTraining.ConsoleApp/ADODotNetExample.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKMSDotNetTraining.ConsoleApp
+{
+    public class ADODotNetExample
+    {
+        private readonly string _connectionString = "Data Source=localhost;Initial Catalog=CKMSDotNetTraining;User ID=sa;Password=YourPassword123!";
+        
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+            String query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_blog] Where [DeleteFlag] = 0
+";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("BlogId: " + reader["BlogId"]);
+                Console.WriteLine("BlogTitle: " + reader["BlogTitle"]);
+                Console.WriteLine("BlogAuthor: " + reader["BlogAuthor"]);
+                Console.WriteLine("BlogContent: " + reader["BlogContent"]);
+
+                Console.WriteLine();
+            }
+            //DataTable dt = new DataTable();
+            //adapter.Fill(dt); 
+
+            //foreach( DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine("BlogTilte"+dr["BlogTitle"]);
+            //    Console.WriteLine("BlogAuthor"+dr["BlogAuthor"]);
+            //    Console.WriteLine("BlogContent"+dr["BlogContent"]);
+
+            //} 
+            connection.Close();
+
+
+            Console.WriteLine("Connection Closed.... ");
+
+        }
+
+        public void Create()
+        {
+            Console.Write("Enter Title :");
+            String blogTitle = Console.ReadLine();
+            Console.Write("Enter Author :");
+            String blogAuthor = Console.ReadLine();
+            Console.Write("Enter Content :");
+            String blogContent = Console.ReadLine();
+
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            String query = @"INSERT INTO [dbo].[Tbl_blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent
+           ,0)
+";
+            SqlCommand cmd = new SqlCommand(query,connection);
+            cmd.Parameters.AddWithValue("@BlogTitle", blogTitle);
+            cmd.Parameters.AddWithValue("@BlogAuthor", blogAuthor);
+            cmd.Parameters.AddWithValue("@BlogContent", blogContent);
+
+            int result = cmd.ExecuteNonQuery();
+            Console.WriteLine(result == 1 ? "Save successfully !" : "Save fail !");
+
+
+            connection.Close();
+
+        }
+      
+    }
+}
--- CKMSDotNetTraining.ConsoleApp/Program.cs
+++ CKMSDotNetTraining.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CKMSDotNetTraining.ConsoleApp;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
@@ -7,42 +8,7 @@ Console.WriteLine("Hello, World!");
 //Console.ReadLine();
 
 // md = markdown
-
-String connectionString = "Data Source=localhost;Initial Catalog=CKMSDotNetTraining;User ID=sa;Password=YourPassword123!";
-Console.WriteLine("Connection String "+ connectionString);
-SqlConnection connection = new SqlConnection(connectionString);
-Console.WriteLine("Connection Opening.... ");
-connection.Open();
-Console.WriteLine("Connection Opened.... ");
-
-String query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_blog] Where [DeleteFlag] = 0
-";
-
-SqlCommand cmd = new SqlCommand(query,connection);
-SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-       Console.WriteLine("BlogId: " + reader["BlogId"]);
-    Console.WriteLine("BlogTitle: " + reader["BlogTitle"]);
-    Console.WriteLine("BlogAuthor: " + reader["BlogAuthor"]);
-    Console.WriteLine("BlogContent: " + reader["BlogContent"]);
-   
-    Console.WriteLine();
-}
-//DataTable dt = new DataTable();
-//adapter.Fill(dt); 
-
-//foreach( DataRow dr in dt.Rows)
-//{
-//    Console.WriteLine("BlogTilte"+dr["BlogTitle"]);
-//    Console.WriteLine("BlogAuthor"+dr["BlogAuthor"]);
-//    Console.WriteLine("BlogContent"+dr["BlogContent"]);
-
-//}
+ADODotNetExample adodotnet = new ADODotNetExample();
+//adodotnet.Read();
+adodotnet.Create();
 Console.ReadKey();
