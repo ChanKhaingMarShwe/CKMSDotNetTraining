0cb10f7c5215a9c310ebcd6130e2233e90a7a518
Khaing Mar Shwe <khaingmarshwe@acedatasystems.com>
2025-06-17 10:07:21 +0630

    feat : ApexChart - PieChart and Mix Chart

---
+++ CKMSDotNetTraining.ChartWebApp/Models/ApexChartMixedChartModel.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CKMSDotNetTraining.ChartWebApp.Models
+{
+
+    public class ApexChartSeriesModel
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int[] Data { get; set; } // Must not be null!
+    }
+
+    public class ApexChartMixedChartModel
+    {
+        public List<ApexChartSeriesModel> Series { get; set; }
+        public string[] Labels { get; set; }
+
+
+    }
+
+
+}
+
+
---
+++ CKMSDotNetTraining.ChartWebApp/Models/ApexChartPieChartModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CKMSDotNetTraining.ChartWebApp.Models
+{
+    public class ApexChartPieChartModel
+    {
+      public  int[] Series { get; set; } 
+      public string[] Labels { get; set; } 
+    }
+
+
+
+}
